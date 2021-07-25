5ffd0b4952c9cd581b93f283ca5c53014fdb3c14
Keenan06 <78598120+Keenan06@users.noreply.github.com>
2021-07-25 14:26:15 +0200

    Add files via upload

---
+++ demo/src/App.js
@@ -0,0 +1,44 @@
+import React, {Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+import NewComponent from './NewComponent';
+import users from './data/data';
+
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      users
+    }
+  }
+
+getName = (num) => {
+const {users} = this.state;
+ return users[num].name;
+ }
+
+  getRandomName = () => {
+    const {users} = this.state;
+    return users[this.random()].name;
+  }
+
+  random = () => {
+    const n = Math.floor((Math.random() * this.state.users.length) + 1);
+    return n;
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <NewComponent username={this.getRandomName()} />
+      </div>
+    ); 
+  }
+}
+
+export default App;
---
+++ demo/src/data/data.js
@@ -0,0 +1,34 @@
+const users = [
+    {
+        "id": 1,
+        "name": "Leanne Graham",
+        "username": "Bret"
+    },
+    {
+        "id": 2,
+        "name": "Sheldon Sawers",
+        "username": "Shell"
+    },
+    {
+        "id": 3,
+        "name": "Scott Man",
+        "username": "Scotty"
+    },
+    {
+        "id": 4,
+        "name": "Jaymee Soul",
+        "username": "Jay"
+    },
+    {
+        "id": 5,
+        "name": "Trystan Dames",
+        "username": "Trys"
+    },
+    {
+        "id": 6,
+        "name": "Kyle Vosloo",
+        "username": "Kyle"
+    }
+];
+
+export default users;
